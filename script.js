354eebdfd62c665f14f5332b600e4c17c257f1fb
blint <korpaibalint@gmail.com>
2024-03-17 21:00:41 +0100

    Final

--- script.js
+++ script.js
@@ -4,17 +4,27 @@ function ValidateForm() {
     /^[a-z]([a-z0-9]*[._-]{0,1}[a-z0-9]+){3,}@[a-z]([a-z0-9]*[-]{0,1}[a-z0-9]+){3,}\.[a-z]{2,4}$/;
 
   if (email.value == "") {
-    SetErrorStat(email, "Email must not be empty");
+    SetEmailError(email, "Email must not be empty");
+    return false;
   } else if (!email.value.match(emailValidatorRegex)) {
-    SetErrorStat(email, "Valid email required");
-  } else {
-    document.getElementById("subscribedEmail").innerHTML = email.value;
-    document.getElementById("formSection").classList.add("form-valid");
+    SetEmailError(email, "Valid email required");
+    return false;
   }
+  document.getElementById("subscribedEmail").innerHTML = email.value;
+  document.getElementById("formSection").classList.add("form-valid");
+  ResetEmailError(email);
   return false;
 }
 
-function SetErrorStat(email, errorText) {
+function ResetEmailError(email) {
+  email.classList.remove("email-error");
+  email.value = "";
+  const emailError = document.getElementById("emailError");
+  emailError.innerHTML = "";
+  emailError.classList.remove("email-error");
+}
+
+function SetEmailError(email, errorText) {
   email.classList.add("email-error");
   const emailError = document.getElementById("emailError");
   emailError.innerHTML = errorText;
